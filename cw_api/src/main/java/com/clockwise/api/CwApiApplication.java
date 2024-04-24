d27595a617308e2a9e012bab2551b3a0158faf29
François Termote <francois.termote@gmail.com>
2024-04-24 18:12:51 +0200

    test

--- cw_api/src/main/java/com/clockwise/api/CwApiApplication.java
+++ cw_api/src/main/java/com/clockwise/api/CwApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CwApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CwApiApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
